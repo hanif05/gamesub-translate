19f116c151aa297a9648c3b3b11d04e879261eb9
hanif05 <hanif05@local>
2026-08-02 09:21:09 +0700

    T5: OcrEngine (Tesseract wrapper, singleton-style reuse)

--- src/GameSubTranslate.Prototype/Program.cs
+++ src/GameSubTranslate.Prototype/Program.cs
@@ -1,4 +1,20 @@
-using GameSubTranslate.Prototype;
+using System.Drawing;
+using System.Drawing.Imaging;
+using GameSubTranslate.Prototype.Ocr;
 
-var a = CliArgsParser.Parse(args);
-Console.WriteLine($"x={a.X} y={a.Y} w={a.W} h={a.H} interval={a.IntervalMs}ms");
+// Generate a sample image with text to OCR
+using (var bmp = new Bitmap(400, 80))
+{
+    using (var g = Graphics.FromImage(bmp))
+    {
+        g.Clear(Color.White);
+        using var font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Regular);
+        g.DrawString("Hello world 123", font, Brushes.Black, 10, 20);
+    }
+    bmp.Save("sample.png", ImageFormat.Png);
+}
+
+byte[] png = File.ReadAllBytes("sample.png");
+using var engine = new TesseractOcrEngine();
+string text = engine.Recognize(png);
+Console.WriteLine($"OCR: '{text}' (len={text.Length})");
