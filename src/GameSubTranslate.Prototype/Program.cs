f57acb3e36e3520d91049fda0fcb4045796baf05
hanif05 <hanif05@local>
2026-08-02 09:19:18 +0700

    T3: ScreenCapture module (GDI+ CaptureRegion)

--- src/GameSubTranslate.Prototype/Program.cs
+++ src/GameSubTranslate.Prototype/Program.cs
@@ -1,1 +1,10 @@
+using System.Drawing;
+using GameSubTranslate.Prototype.Capture;
+
 Console.WriteLine("hello");
+
+var png = ScreenCapture.CaptureRegion(0, 0, 200, 100);
+File.WriteAllBytes("test.png", png);
+
+using var img = Image.FromFile("test.png");
+Console.WriteLine($"test.png: {img.Width}x{img.Height}, {png.Length} bytes");
